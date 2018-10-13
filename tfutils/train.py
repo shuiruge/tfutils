c80b08e31feac12ad8aefccc8e98156301e43f72
pen. ciun. jian <shuiruge@whu.edu.cn>
2018-10-13 17:20:23 +0800

    Rename the scope-checkpoiont. Add `ensure_directory`.

--- tfutils/train.py
+++ tfutils/train.py
@@ -1,5 +1,6 @@
 import os
 import tensorflow as tf
+from tensorflow.python.lib.io import file_io
 
 
 def clip(gradient, min_clip_value=-1e+0,  max_clip_value=1e+0):
@@ -23,38 +24,52 @@ def clip(gradient, min_clip_value=-1e+0,  max_clip_value=1e+0):
   return tf.clip_by_value(gradient, min_clip_value, max_clip_value)
 
 
-def save_variables(session, scope, save_dir):
-    """Saves the trained variables within the scope from session
-    to disk.
-    
-    Args:
-        session: An instance of `tf.Session`.
-        scope: String.
-        save_dir: String.
-    """
-    pretrained_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
-                                        scope=scope)
-    saver = tf.train.Saver(pretrained_vars)
+def get_ckpt_name(scope):
+  """Auxillary funtion.
 
-    ckpt_path = os.path.join(save_dir, '{}_scope.ckpt'.format(scope))
-    saver.save(session, ckpt_path)
+  Argus:
+    scope: String or `None`.
+  """
+  if scope is None:
+    return 'all_scopes.ckpt'
+  return  '{}_scope.ckpt'.format(scope)
+
+
+def save_variables(session, scope, save_dir):
+  """Saves the trained variables within the scope from session
+  to disk.
+    
+  Args:
+    session: An instance of `tf.Session`.
+    scope: String or `None`.
+    save_dir: String. This directory can be non-exist. If not exists,
+      then calling this function will create one.
+  """
+  ensure_directory(save_dir)
+
+  pretrained_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+                                      scope=scope)
+  saver = tf.train.Saver(pretrained_vars)
+
+  ckpt_path = os.path.join(save_dir, get_ckpt_name(scope))
+  saver.save(session, ckpt_path)
 
 
 def restore_variables(session, scope, save_dir):
-    """Restores the pre-trained variables within the scope from disk
-    to session. Cooperating with the function `save_vars`.
-    
-    Args:
-        session: An instance of `tf.Session`.
-        scope: String.
-        save_dir: String.
-    """
-    pretrained_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+  """Restores the pre-trained variables within the scope from disk
+  to session. Cooperating with the function `save_vars`.
+  
+  Args:
+    session: An instance of `tf.Session`.
+    scope: String.
+    save_dir: String. This directory shall exist.
+  """
+  pretrained_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
                                         scope=scope)
-    saver = tf.train.Saver(pretrained_vars)
-
-    ckpt_path = os.path.join(save_dir, '{}_scope.ckpt'.format(scope))
-    saver.restore(session, ckpt_path)
+  saver = tf.train.Saver(pretrained_vars)
+    
+  ckpt_path = os.path.join(save_dir, get_ckpt_name(scope))
+  saver.restore(session, ckpt_path)
 
 
 def create_frugal_session(gpu_allocation=0.1):
@@ -95,3 +110,18 @@ def smear(values, window_size):
 
         smeared.append(np.mean(values_in_window))
     return smeared
+
+ 
+def ensure_directory(directory):
+  """Checks if the directory exists, and if not so, create one.
+
+  Args:
+    directory: String.
+
+  Returns:
+    Boolean, being `True` if exists, otherwise `False`.
+  """
+  if not file_io.file_exists(directory):
+    file_io.recursive_create_dir(directory)
+    return True
+  return False
