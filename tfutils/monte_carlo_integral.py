e2be2ac732c797724125490d839c54d754b134aa
pen. ciun. jian <shuiruge@whu.edu.cn>
2018-10-21 14:57:33 +0800

    Add mc_mean_with_nan.

--- tfutils/monte_carlo_integral.py
+++ tfutils/monte_carlo_integral.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 
 class MonteCarloIntegral(object):
   """
@@ -24,3 +26,36 @@ class MonteCarloIntegral(object):
   @property
   def dtype(self):
     return self.value.dtype
+
+
+def mc_mean_with_nan(samples):
+  """Returns the Monte-Carlo-mean of `samples` wherein there may be NaN.
+
+  This function is useful at the starting epoch of training, wherein the
+  initialized distributions may be far from compatible with some of the
+  provided data.
+
+  However, this function may hide the numerical instability that is
+  intrinsic to the model to be trained. So, it is NOT suggested to use
+  this function!
+
+  Args:
+    samples: Tensor with shape `[n_samples] + rest_shapes`, for any
+      `rest_shapes`.
+
+  Returns:
+    Tensor with shape `rest_shapes`.
+  """
+  # [n_samples] + rest_shape
+  finite_sample_mask = tf.where(tf.is_nan(samples),
+                                tf.zeros_like(samples),
+                                tf.ones_like(samples))
+  # [1] + rest_shape
+  finite_sample_count = tf.reduce_sum(finite_sample_mask,
+                                      axis=0, keepdims=True)
+  # [n_samples] + rest_shape
+  finite_samples = tf.where(tf.is_nan(samples),
+                            tf.zeros_like(samples),
+                            samples)
+  return tf.reduce_sum(finite_samples / (finite_sample_count + 1e-8),
+                       axis=0)
