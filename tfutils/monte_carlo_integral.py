4a1bcb9090b2f8acd1c5ed38fdbd17093c324c47
pen. ciun. jian <shuiruge@whu.edu.cn>
2018-11-16 12:22:30 +0800

    Add monte_carlo_integrate.

--- tfutils/monte_carlo_integral.py
+++ tfutils/monte_carlo_integral.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 
@@ -59,3 +60,64 @@ def mc_mean_with_nan(samples):
                             samples)
   return tf.reduce_sum(finite_samples / (finite_sample_count + 1e-8),
                        axis=0)
+
+
+def monte_carlo_integrate(
+        integrands,
+        axes=[0],
+        name='monte_carlo_integrate'):
+  r"""
+  Definition:
+    ```math
+    Let $x \in \mathcal{X}$ sampled from measurement $P_X$. The integral
+    $\int_{\mathcal{X}} dx p(x) f(x)$ can be approximated by Monte-Carlo
+    integral
+
+    \begin{equaiton}
+        \int_{\mathcal{X}} dx p(x) f(x) \approx
+            \mathbb{E}_{x \sim P_X} \left[ f(x) \right],
+    \end{equation}
+
+    which carries an relative error
+
+    $$\sqrt{\frac{\textrm{Var}_{x \sim P_X} \left[ f(x) \right]}{N}},$$
+
+    where $N$ the number of samples. The set $\{ f(x) \mid x \sim P_X \}$
+    is called "integrands" herein.
+    ```
+
+  Examples:
+    >>> # Example 1
+    >>> # To integrate f(x) := x^2
+    >>> xs = tf.random_uniform(shape=(128), dtype='float32')
+    >>> integrands = tf.square(xs)
+    >>> mc_int = monte_carlo_integrate(integrands, axes=[0])
+    >>> #
+    >>> # Example 2
+    >>> # To integrate f(x) := x^2, but with non-trivial `axes`
+    >>> xs = tf.random_uniform(shape=(16, 8), dtype='float32')
+    >>> integrands = tf.square(xs)
+    >>> mc_int = monte_carlo_integrate(integrands, axes=[0, 1])
+
+  Args:
+    integrand: Tensor.
+    axes: Iterable of non-negative integers, as the axes to be integrated
+      over.
+    name: String.
+
+  Returns:
+    A `GANLoss` instance.
+  """
+  with tf.name_scope(name):
+    with tf.name_scope('n_samples'):
+      integrands_shape = integrands.get_shape().as_list()
+
+      sample_shape = []
+      for axis in axes:
+        sample_shape.append(integrands_shape[axis])
+
+      n_samples = np.product(sample_shape)
+      n_samples = tf.constant(n_samples, dtype=integrands.dtype)
+
+    mean, var = tf.nn.moments(integrands, axes)
+    return MonteCarloIntegral(value=mean, error=tf.sqrt(var / n_samples))
