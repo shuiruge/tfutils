43a540a9157fe38ea4bb745457766aa7e285d962
pen. ciun. jian <shuiruge@whu.edu.cn>
2018-12-31 18:37:32 +0800

    Update pyutils.py

--- tfutils/pyutils.py
+++ tfutils/pyutils.py
@@ -25,7 +25,7 @@ def inheritdocstring(cls):
         if base.__name__ in ('abc.ABC', 'ABC', 'object'):
             continue
         if base.__doc__ is not None:
-            cls.__doc__ += ('\n\n  Inheriting from class {0}:\n\n{1}'
+            cls.__doc__ += ('\n\nInheriting from class {0} --\n\n{1}'
                             .format(base.__name__, base.__doc__))
 
     methods = [attr_name for attr_name in dir(cls)
@@ -42,7 +42,7 @@ def inheritdocstring(cls):
             if not hasattr(base, method):
                 continue
             base_docstring = getattr(base, method).__doc__
-            if base_docstring:
+            if base_docstring and base_docstring != docstring:
                 docstring = base_docstring + '\n\n' + docstring
         return docstring
 
@@ -69,3 +69,42 @@ def is_method(cls, attr_name):
     if hasattr(attr, '__call__'):  # thus being a method.
         return True
     return False
+
+
+if __name__ == '__main__':
+
+    """Tests"""
+
+    class A(object):
+        """This is A."""
+
+        def method_1(self):
+            """This is method_1."""
+            pass
+
+    @inheritdocstring
+    class B(A):
+        """This is B."""
+        pass
+
+    @inheritdocstring
+    class C(A):
+        """This is C."""
+        def method_1(self):
+            """Override."""
+            pass
+
+        def method_2(self):
+            """New method."""
+
+    print(B.__doc__)
+    print('\n\n\n')
+
+    print(B.method_1.__doc__)
+    print('\n\n\n')
+
+    print(C.method_1.__doc__)
+    print('\n\n\n')
+
+    print(C.method_2.__doc__)
+    print('\n\n\n')
