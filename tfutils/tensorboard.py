5f1890b691348016523ca8aeb9a69d0cebd3df27
pen. ciun. jian <shuiruge@whu.edu.cn>
2019-01-19 14:43:59 +0800

    Create tensorboard.py

---
+++ tfutils/tensorboard.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+
+def variable_summaries(var, name='summaries'):
+  """Attaches a lot of summaries to a Tensor `var` (for TensorBoard
+  visualization).
+
+  Forked from: https://www.tensorflow.org/guide/summaries_and_tensorboard
+
+  Args:
+    var: Tensor.
+    name: String.
+  """
+  with tf.name_scope(name):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean', mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev', stddev)
+    tf.summary.scalar('max', tf.reduce_max(var))
+    tf.summary.scalar('min', tf.reduce_min(var))
+    tf.summary.histogram('histogram', var)
