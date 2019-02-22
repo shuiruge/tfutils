b5dc3dc316cf8bf9b0e284392129485aa1aa04c6
pen. ciun. jian <shuiruge@whu.edu.cn>
2019-02-22 14:48:02 +0800

    Update tensorboard.py

--- tfutils/tensorboard.py
+++ tfutils/tensorboard.py
@@ -12,11 +12,17 @@ def variable_summaries(var, name='summaries'):
     name: String.
   """
   with tf.name_scope(name):
-    mean = tf.reduce_mean(var)
-    tf.summary.scalar('mean', mean)
-    with tf.name_scope('stddev'):
-      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-    tf.summary.scalar('stddev', stddev)
-    tf.summary.scalar('max', tf.reduce_max(var))
-    tf.summary.scalar('min', tf.reduce_min(var))
     tf.summary.histogram('histogram', var)
+
+    shape = var.get_shape().as_list()
+    if not shape:  # scalar.
+      tf.summary.scalar('mean', var)
+
+    else:  # multi-component tensor
+      mean = tf.reduce_mean(var)
+      tf.summary.scalar('mean', mean)
+      with tf.name_scope('stddev'):
+        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+      tf.summary.scalar('stddev', stddev)
+      tf.summary.scalar('max', tf.reduce_max(var))
+      tf.summary.scalar('min', tf.reduce_min(var))
